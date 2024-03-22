2e8b9ffc1db7d206bb8a714cb0d6711232e4e4c3
ramtinteymouri <ramtinteymouri@gmail.com>
2024-03-22 14:28:20 -0700

    Added a list of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert("Hello world");
-let favoriteFood = "Steak";
-document.write(favoriteFood);
+//Ex 1.1
+// alert("Hello world");
+// let favoriteFood = "Steak";
+// document.write(favoriteFood);
+
+//Ex 1.2
+let pokemonList = [{ name: "Meowth", height: 0.4, type: "normal" },
+{ name: "Spearow", height: 0.3, type: ["flying", "normal"] },
+{ name: "Lapras", height: 2.5, type: ["ice", "water"] }];
