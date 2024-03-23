9fd95ce9d56ecbe842732625bd54a412b2c6b925
ramtinteymouri <ramtinteymouri@gmail.com>
2024-03-23 11:59:27 -0700

    added the for look with a condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-let pokemonList = [{ name: "Meowth", height: 0.4, type: "normal" },
-{ name: "Spearow", height: 0.3, type: ["flying", "normal"] },
-{ name: "Lapras", height: 2.5, type: ["ice", "water"] }];
+let pokemonList = [
+    { name: "Meowth", height: 0.4, type: "normal" },
+    { name: "Spearow", height: 0.3, type: ["flying", "normal"] },
+    { name: "Lapras", height: 2.5, type: ["ice", "water"] }
+];
+// for loop to iterate over each Pokemon
+for (i = 0; i < pokemonList.length; i++) {
+    document.write(`<p>${pokemonList[i].name} (height ${pokemonList[i].height}m)`)
+    // if-else statement to highlight the large Pokemon
+    if (pokemonList[i].height > 2) {
+        document.write(" - WOW That's huge!</p>")
+    }
+    else {
+        document.write("</p>")
+    }
+}
