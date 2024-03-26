302c744f05cd02bce04ec80a05997e44647f3821
ramtinteymouri <ramtinteymouri@gmail.com>
2024-03-26 13:50:58 -0700

    updated the code with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
-let pokemonList = [
-    { name: "Meowth", height: 0.4, type: "normal" },
-    { name: "Spearow", height: 0.3, type: ["flying", "normal"] },
-    { name: "Lapras", height: 2.5, type: ["ice", "water"] }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: "Meowth", height: 0.4, type: "normal" },
+        { name: "Spearow", height: 0.3, type: ["flying", "normal"] },
+        { name: "Lapras", height: 2.5, type: ["ice", "water"] }
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 // forEach loop to replace the previous for loop
-pokemonList.forEach(item => {
+pokemonRepository.getAll().forEach(item => {
     document.write(`<p>${item.name} (height ${item.height}m)`)
     // if-else statement to highlight the large Pokemon
     if (item.height > 2) {
