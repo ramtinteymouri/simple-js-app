a7a27fc505b372f235676b26ba002dc7ca6db597
ramtinteymouri <ramtinteymouri@gmail.com>
2024-03-26 13:37:28 -0700

    replaced for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ let pokemonList = [
     { name: "Spearow", height: 0.3, type: ["flying", "normal"] },
     { name: "Lapras", height: 2.5, type: ["ice", "water"] }
 ];
-// for loop to iterate over each Pokemon
-for (i = 0; i < pokemonList.length; i++) {
-    document.write(`<p>${pokemonList[i].name} (height ${pokemonList[i].height}m)`)
+// forEach loop to replace the previous for loop
+pokemonList.forEach(item => {
+    document.write(`<p>${item.name} (height ${item.height}m)`)
     // if-else statement to highlight the large Pokemon
-    if (pokemonList[i].height > 2) {
+    if (item.height > 2) {
         document.write(" - WOW That's huge!</p>")
     }
     else {
         document.write("</p>")
     }
-}
+})
